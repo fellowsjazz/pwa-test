2eaf7c8095a18888ef9d62847c70c552107e7651
fellowsjazz <russellmatthews32@gmail.com>
2023-08-19 20:04:16 -0400

    prod error fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const runtimeCaching = require("next-pwa/cache");
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching,
+})
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
